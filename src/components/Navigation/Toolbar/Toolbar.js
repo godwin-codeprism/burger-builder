7a1d8c99a89268a597eff4d5e91dd03f10cd69a9
godwinvc <godwin@swift.local>
2017-12-23 17:47:50 +0530

    Completed Burger Builder and Optimzed

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Aux from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Logo from '../../Logo/Logo';
 
 import NavigationItems from '../NavigationItems/NavigationItems';
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Auxiliary';
-import ToolBar from '../Navigation/Toolbar/Toolbar';
+import Aux from '../Auxiliary/Auxiliary';
+import ToolBar from '../../components/Navigation/Toolbar/Toolbar';
 import classes from './Layout.css';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 class Layout extends Component {
     state = {
         SideDrawerClosed: false
